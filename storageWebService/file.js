10d495bc0d3b3ac4247f534ed2575fe5232ef691
Nitish Bajaj <nitish111@hotmail.com>
2019-10-09 23:41:55 -0400

    Added data

--- storageWebService/file.js
+++ storageWebService/file.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var file = new Schema({
-    "name": { type: String, unique: true },
-    "fileType": String,
-    "fileSize": String,
-    "dateUploaded": String,
-    "dateModified": String    
-  });
+  "name": { type: String, unique: true },
+  "fileType": String,
+  "fileSize": String,
+  "dateUploaded": String,
+  "dateModified": String    
+});
   
 module.exports = file;
