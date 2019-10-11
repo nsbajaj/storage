385279c4de894e2be4d57ae47697c709d10a2261
Nitish Bajaj <nitish111@hotmail.com>
2019-10-10 22:33:09 -0400

    Added routing and navbar

--- storageApp/src/app/app.module.ts
+++ storageApp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
